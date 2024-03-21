67e4a65dd14e7f4f6da1e43d4f145c6781e7b032
fmagana14 <142852197+fmagana14@users.noreply.github.com>
2024-03-21 15:43:46 -0700

    adding middleware

--- app.js
+++ app.js
@@ -1,41 +1,46 @@
 // Require Libraries
-const express = require('express');
+const express = require("express");
 
 // App Setup
 const app = express();
 
 // Middleware
 // Allow Express (our web framework) to render HTML templates and send them back to the client using a new function
-const handlebars = require('express-handlebars');
+const handlebars = require("express-handlebars");
 
 const hbs = handlebars.create({
-    // Specify helpers which are only registered on this instance.
-    helpers: {
-        foo() { return 'FOO!'; },
-        bar() { return 'BAR!'; }
-    }
+  // Specify helpers which are only registered on this instance.
+  helpers: {
+    foo() {
+      return "FOO!";
+    },
+    bar() {
+      return "BAR!";
+    },
+  },
 });
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-app.set('views', './views');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 
 // Routes
-app.get('/', (req, res) => {
-    // set the url of the gif
-    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-    // render the hello-gif view, passing the gifUrl into the view to be displayed
-    res.render('hello-gif', { gifUrl })
-  })
+app.get("/", (req, res) => {
+  // set the url of the gif
+  const gifUrl =
+    "https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245";
+  // render the hello-gif view, passing the gifUrl into the view to be displayed
+  res.render("hello-gif", { gifUrl });
+});
 
-  app.get('/greetings/:name', (req, res) => {
-    // grab the name from the path provided
-    const name = req.params.name;
-    // render the greetings view, passing along the name
-    res.render('greetings', { name });
-  })
+app.get("/greetings/:name", (req, res) => {
+  // grab the name from the path provided
+  const name = req.params.name;
+  // render the greetings view, passing along the name
+  res.render("greetings", { name });
+});
 // Start Server
 
 app.listen(3000, () => {
-  console.log('Gif Search listening on port localhost:3000!');
+  console.log("Gif Search listening on port localhost:3000!");
 });
