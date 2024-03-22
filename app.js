a1a10e6de1caa421cc09f7d83b21b7ba6400cd50
fmagana14 <142852197+fmagana14@users.noreply.github.com>
2024-03-21 18:31:11 -0700

    added search form

--- app.js
+++ app.js
@@ -25,13 +25,10 @@ app.set("view engine", "handlebars");
 app.set("views", "./views");
 
 // Routes
-app.get("/", (req, res) => {
-  // set the url of the gif
-  const gifUrl =
-    "https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245";
-  // render the hello-gif view, passing the gifUrl into the view to be displayed
-  res.render("hello-gif", { gifUrl });
-});
+app.get("/", (req,res) => {
+  console.log(req.query)
+  res.render("home")
+})
 
 app.get("/greetings/:name", (req, res) => {
   // grab the name from the path provided
